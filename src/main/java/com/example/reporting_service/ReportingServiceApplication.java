e166ffadab9d9bf4c0a8bf235b7da9da35369f9d
Bienvenu2004 <kidsglory1@gmail.com>
2025-05-27 15:41:02 +0100

    feat: added hello endpoint

--- src/main/java/com/example/reporting_service/ReportingServiceApplication.java
+++ src/main/java/com/example/reporting_service/ReportingServiceApplication.java
@@ -2,12 +2,21 @@ package com.example.reporting_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ReportingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportingServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
---
+++ src/main/java/com/example/reporting_service/controller/ReportingController.java
@@ -0,0 +1,19 @@
+package com.example.reporting_service.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/transaction")
+@CrossOrigin(origins = "*")
+public class ReportingController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from reporting Service!";
+    }
+}
